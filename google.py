95bcc8514201b18ff1b28be9403a7519b9676142
Chang Won Choi <cchang0705@gmail.com>
2020-08-09 22:25:09 +0900

    Replaced upload image's container with the webcam cotainer

--- google.py
+++ google.py
@@ -2,6 +2,7 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"smiling,frowning,angry,sticking tongue out,winking,kissing face","suffix_keywords":"person","limit":100,"format":"jpg","print_urls":False}   #creating list of arguments
+arguments = {"keywords":"화난 사람 얼굴, 웃는 사람 얼굴, 메롱 얼굴","limit":100,"format":"jpg","print_urls":False,"color_type":"black-and-white"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
+
